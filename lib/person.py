a3909f85a2766e5aac0ee8f4fd209620915dd942
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-05-16 12:31:32 +0300

    commit

--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,31 @@
-#!/usr/bin/env python3
-
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
+approved_jobs = [
+    "Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance",
+    "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Jane Doe", job="Sales"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job in approved_jobs:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
